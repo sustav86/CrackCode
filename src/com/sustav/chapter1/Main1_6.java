58f9c2f393621b69979ee819ea5806368237f42b
Sustavov Anton <Anton.Sustavov@alfabank.kiev.ua>
2019-04-18 16:21:45 +0300

    ratate matrix in place

---
+++ src/com/sustav/chapter1/Main1_6.java
@@ -0,0 +1,63 @@
+package com.sustav.chapter1;
+
+import java.util.Random;
+
+public class Main1_6 {
+
+    private static Random random = new Random();
+
+    public static void main(String[] args) {
+//        int[][] matrix = new int[3][3];
+        int[][] matrix = {
+                {11, 12, 13},
+                {21, 22, 23},
+                {31, 32, 33}
+        };
+        printMatrix(matrix);
+        System.out.println();
+        rotate(matrix, 3);
+        printMatrix(matrix);
+//        fill(matrix);
+    }
+
+    private static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static void fill(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = random.nextInt(10);
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void rotate(int[][] matrix, int n) {
+        for (int layer = 0; layer < n / 2; ++layer) {
+            int first = layer;
+            int last = n - 1 - layer;
+            for (int i = first; i < last; ++i) {
+                int offset = i - first;
+                int top = matrix[first][i]; // save top
+                // left -> top
+                matrix[first][i] = matrix[last - offset][first];
+
+                // bottom -> left
+                matrix[last - offset][first] = matrix[last][last - offset];
+
+                // right -> bottom
+                matrix[last][last - offset] = matrix[i][last];
+
+                // top -> right
+                matrix[i][last] = top; // right <- saved top
+            }
+        }
+    }
+}
